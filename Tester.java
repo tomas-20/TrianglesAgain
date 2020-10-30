d6dc6a63c128a65af11b25ff04dc49f52151928a
tomas-20 <tacuna20@stuy.edu>
2020-10-29 20:53:51 -0400

    did the copy paste

--- Tester.java
+++ Tester.java
@@ -1,5 +1,8 @@
 public class Tester {
   public static void main(String[] args) {
-    
+    Point bob = new Point(1, 2);
+    Point joe = new Point(bob);
+    System.out.println(joe.getX());
+    System.out.println(joe.getY());
   }
 }
