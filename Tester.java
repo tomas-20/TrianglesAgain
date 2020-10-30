361e37758b17da46c08b193475371c6c3e34f5bc
tomas-20 <tacuna20@stuy.edu>
2020-10-29 23:13:55 -0400

    made the triangle functions

--- Tester.java
+++ Tester.java
@@ -10,5 +10,7 @@ public class Tester {
     Point dylan = new Point(8, 9);
     Triangle pythagoras = new Triangle(bob, dylan, joe);
     Triangle euclid = new Triangle(2, 1, 8, 7, 4, 2);
+    System.out.println(euclid.getPerimeter());
+    System.out.println(euclid.getArea());
   }
 }
--- Triangle.java
+++ Triangle.java
@@ -10,4 +10,18 @@ public class Triangle {
     v2 = new Point(x2, y2);
     v3 = new Point(x3, y3);
   }
+  private double[] getSides() {
+    return new double[] {v1.distanceTo(v2), v2.distanceTo(v3), v3.distanceTo(v1)};
+  }
+  private double sum(double[] arr) {
+    return arr[0] + arr[1] + arr[2];
+  }
+  public double getPerimeter() {
+    return sum(getSides());
+  }
+  public double getArea() {
+    double[] sides = getSides();
+    double s = sum(sides) / 2;
+    return Math.pow(s * (s - sides[0]) * (s - sides[1]) * (s - sides[2]), 0.5);
+  }
 }
